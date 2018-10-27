849e6b4b2db7f272d62cdf4ca2d03a7e29720b3b
Peiren Yang <yangpeiren1217@gmail.com>
2018-10-27 14:38:06 +0200

    added new obj processing script

--- franka.py
+++ franka.py
@@ -5,7 +5,8 @@ import pybullet as p
 class Franka:
 
     def __init__(self, urdf_path, pos=(0, 0, 0), orn=(0, 0, 0, 1)):
-        self.robot_id = p.loadURDF(urdf_path, pos, orn, useFixedBase=True, flags=p.URDF_USE_SELF_COLLISION)
+        self._p = p
+        self.robot_id = self._p.loadURDF(urdf_path, pos, orn, useFixedBase=True, flags=self._p.URDF_USE_SELF_COLLISION)
 
         self._motor_count = 7
         self._default_pos = []
@@ -22,30 +23,30 @@ class Franka:
         self._load_robot_info()
 
     def _load_robot_info(self):
-        self.joint_count = p.getNumJoints(self.robot_id)
+        self.joint_count = self._p.getNumJoints(self.robot_id)
         self._end_effector_idx = self.joint_count
 
         self._jd = [1e-5 for _ in range(self.joint_count)]
         self._max_forces = [200 for _ in range(self._motor_count)]
         joint_indices = [i for i in range(self._motor_count)]
-        self._default_pos = [pos[0] for pos in p.getJointStates(self.robot_id, joint_indices)]
+        self._default_pos = [pos[0] for pos in self._p.getJointStates(self.robot_id, joint_indices)]
 
         for i in range(self.joint_count):
-            joint_info = p.getJointInfo(self.robot_id, i)
+            joint_info = self._p.getJointInfo(self.robot_id, i)
             self._motor_lower_limits.append(joint_info[8])
             self._motor_upper_limits.append(joint_info[9])
             self._max_velocities.append(joint_info[11])
 
     def reset(self):
         for i in range(self._motor_count):
-            p.resetJointState(self.robot_id, i, targetValue=self._default_pos[i])
+            self._p.resetJointState(self.robot_id, i, targetValue=self._default_pos[i])
 
     def get_observation(self):
         observation = []
-        state = p.getLinkState(self.robot_id, self._end_effector_idx)
+        state = self._p.getLinkState(self.robot_id, self._end_effector_idx)
         pos, orn = state[0], state[1]
         # Why euler here?
-        euler = p.getEulerFromQuaternion(orn)
+        euler = self._p.getEulerFromQuaternion(orn)
 
         observation.extend(list(pos))
         observation.extend(list(euler))
@@ -62,8 +63,8 @@ class Franka:
                 raise ValueError('The given motor value on axis {0} is out of motor limits'.format(i))
             joint_indices.append(i)
 
-        p.setJointMotorControlArray(self.robot_id, joint_indices, p.POSITION_CONTROL, motor_set_value,
-                                    force=self._max_forces)
+        self._p.setJointMotorControlArray(self.robot_id, joint_indices, self._p.POSITION_CONTROL, motor_set_value,
+                                          force=self._max_forces)
 
 
 if __name__ == '__main__':
