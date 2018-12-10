64730f7b33d895422147a503ad3a11387404cc51
Peiren Yang <yangpeiren1217@gmail.com>
2018-12-10 20:46:48 +0100

    nn structure with pooling layers

--- paint_ppo.py
+++ paint_ppo.py
@@ -1,61 +1,48 @@
-import baselines.ppo2.ppo2 as ppo
 import numpy as np
 import tensorflow as tf
-from baselines.a2c.utils import fc, conv_to_fc
-from baselines.common.models import register
+import ray
+import ray.rllib.agents.ppo as ppo
+from ray.rllib.models import ModelCatalog, Model
+from ray.rllib.models.misc import flatten
 from PaintRLEnv.robot_gym_env import RobotGymEnv
 
 
-hyper_params = dict(
-        nsteps=2048,
-        nminibatches=32,
-        lam=0.95,
-        gamma=0.99,
-        noptepochs=10,
-        log_interval=1,
-        ent_coef=0.0,
-        lr=lambda f: 3e-4 * f,
-        cliprange=0.2,
-        value_network='copy')
+class PaintModel(Model):
+
+    def _build_layers(self, inputs, num_outputs, options):
+        pass
+
+    def _build_layers_v2(self, input_dict, num_outputs, options):
+        conv1 = tf.layers.conv2d(inputs=input_dict['obs']['image'], filters=32, kernel_size=(5, 5), padding='SAME',
+                                 activation=tf.nn.relu, name='conv1')
+        pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=(2, 2), strides=2, name='pool1')
+
+        conv2 = tf.layers.conv2d(inputs=pool1, filters=64, kernel_size=(5, 5), padding='SAME',
+                                 activation=tf.nn.relu, name='conv2')
+        pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=(2, 2), strides=2, name='pool2')
+
+        conv3 = tf.layers.conv2d(inputs=pool2, filters=64, kernel_size=(5, 5), padding='SAME',
+                                 activation=tf.nn.relu, name='conv3')
+        pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=(2, 2), strides=2, name='pool3')
+        pool3 = flatten(pool3)
+        fc = tf.layers.dense(pool3, 64, activation=tf.nn.relu, name='fc')
+        dropout = tf.layers.dropout(fc, tf.constant(0.75), training=input_dict["is_training"], name='dropout')
+        out = tf.layers.dense(dropout, 2, name='logits')
+
+        return out, fc
 
 
-model = ppo.learn(
-    network='conv_with_fc',
-    env=RobotGymEnv,
-    total_timesteps=1e4,
-    **hyper_params
-)
+ModelCatalog.register_custom_model("paint_model", PaintModel)
 
 
-@register('conv_with_fc')
-def conv_with_fc(convs=((32, 8, 4), (64, 4, 2), (64, 3, 1)), fc_layers=2, num_hidden=64, layer_norm=False, **conv_kwargs):
-    """
-    customized for the paint robot task, copied from baselines.common.models
-    :param convs: list of triples (filter_number, filter_size, stride) specifying parameters for each layer.
-    :param fc_layers: int, number of fully-connected layers (default: 2)
-    :param num_hidden: int, size of fully-connected layers (default: 64)
-    :param layer_norm:
-    :param conv_kwargs:
-    :return: function
-    """
+ray.init(num_gpus=1)
 
-    def network_fn(img, pos):
-        out = tf.cast(img, tf.float32) / 255.
-        with tf.variable_scope("conv_fc_net"):
-            for num_outputs, kernel_size, stride in convs:
-                out = tf.contrib.layers.convolution2d(out, num_outputs=num_outputs, kernel_size=kernel_size,
-                                                      stride=stride, activation_fn=tf.nn.relu, **conv_kwargs)
+agent = ppo.PPOAgent(env="CartPole-v0", config={
+    "model": {
+        "custom_model": "paint_model",
+        "custom_options": {},  # extra options to pass to your model
+    },
+})
 
-            # h = tf.contrib.layers.spatial_softmax(out)
-            h = conv_to_fc(out)
-            h = tf.concat(h, pos)
-            for i in range(fc_layers):
-                h = fc(h, 'conv_fc{}'.format(i), nh=num_hidden, init_scale=np.sqrt(2))
-                if layer_norm:
-                    h = tf.contrib.layers.layer_norm(h, center=True, scale=True)
-                h = tf.nn.relu(h)
-            h = fc(h, 'conv_fc{}'.format(fc_layers + 1), nh=num_hidden, init_scale=np.sqrt(2))
-            h = tf.nn.
-        return h
-    return network_fn
+
 
